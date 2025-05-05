8a22de26a0c64c57eedd771d4a0c6da48a3612a7
Badr Eddine El-Harchali <Mr.Elharchali@gmail.com>
2025-05-06 00:00:59 +0100

    Create Task Entity

---
+++ src/main/java/com/badr/task_app/domain/entities/TaskPriority.java
@@ -0,0 +1,7 @@
+package com.badr.task_app.domain.entities;
+
+public enum TaskPriority {
+    LOW,
+    MEDIUM,
+    HIGH
+}
---
+++ src/main/java/com/badr/task_app/domain/entities/TaskStatus.java
@@ -0,0 +1,6 @@
+package com.badr.task_app.domain.entities;
+
+public enum TaskStatus {
+    OPEN,
+    CLOSED
+}
